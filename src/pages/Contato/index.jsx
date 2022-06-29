30732d2ab190509ed85633f0016255ae43c06dd8
Joas Nogueira <joas.nogueira@inema.ba.gov.br>
2022-06-29 13:58:01 -0300

    Criado a pagina Contato

---
+++ src/pages/Contato/index.jsx
@@ -0,0 +1,19 @@
+import'./styles.css'
+
+
+export default function Contato (){
+    return(
+        <>
+            <div className="Conteiner">
+                <div className="Conteudo">
+                    <h1>Contato</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                </div>
+            </div>
+        </>    
+    );
+}
