722e9e4388d7ff6a8e9b54a75eadf325d229fa8d
Joas Nogueira <joas.nogueira@inema.ba.gov.br>
2022-06-29 13:58:22 -0300

    Criado pagina Discord

---
+++ src/pages/Discord/index.jsx
@@ -0,0 +1,19 @@
+import'./styles.css'
+
+
+export default function Discord (){
+    return(
+        <>
+            <div className="Conteiner">
+                <div className="Conteudo">
+                    <h1>discord</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                    <h1>blabla</h1>
+                </div>
+            </div>
+        </>    
+    );
+}
