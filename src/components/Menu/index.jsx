f00c4cd028219538195c2fec4de9da28864834b4
Joas Nogueira <joas.nogueira@inema.ba.gov.br>
2022-06-29 13:57:31 -0300

    Adaptado o menu para as rotas

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './styles.css'
 
 
@@ -7,13 +8,13 @@ export default function Menu(){
     return(
             <nav className="navMenu">
                 <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Novels</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Contato</a></li>
-                    <li><a href="#">Donations</a></li>
-                    <li><a href="#">Discord</a></li>
-                    <li><a href="#">Login</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="novels">Novels</Link></li>
+                    <li><Link to="sobre">Sobre</Link></li>
+                    <li><Link to="contato">Contato</Link></li>
+                    <li><Link to="donations">Donations</Link></li>
+                    <li><Link to="discord">Discord</Link></li>
+                    <li><Link to="login">Login</Link></li>
                 </ul>
             </nav>
     );
